f74196085e8808997999f9066e03d630bf987213
Ander-Ferreira <andersonadm1314@gmail.com>
2024-08-15 21:52:11 -0300

    Aulas next.js inicio

---
+++ fundamentos/src/app/components/Cabecalho.js
@@ -0,0 +1,12 @@
+export default function Cabecalho() {
+    return (
+        <> 
+            <div>
+                <Cabecalho/>
+                <h1>Cabeçalho</h1>
+                <p>Isto é o cabeçalho da minha página</p>
+
+            </div>
+        </>
+    )
+}
---
+++ fundamentos/src/app/fundamentos/page.js
@@ -0,0 +1,18 @@
+import Cabecalho from "../components/Cabecalho";
+
+export default function Fundamentos() {
+
+    
+
+
+
+    return (
+        <>
+            <h1> Fundamentos</h1>
+            <p>Sucesso</p>
+            <Cabecalho/>
+        </>
+
+    )
+
+}
