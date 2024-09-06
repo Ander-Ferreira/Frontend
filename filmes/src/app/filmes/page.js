44c71feed573489751230176241eebfd3bbaa154
Ander-Ferreira <andersonadm1314@gmail.com>
2024-09-05 21:49:35 -0300

    Comitando aula de integração Api do the movie db

---
+++ filmes/src/app/filmes/page.js
@@ -0,0 +1,58 @@
+
+
+'use client'
+import Pagina from "@/app/components/Pagina";
+import apiMovie from "@/services/apiMovies";
+
+import { useEffect, useState } from "react";
+import { Button, Card, Col, Row } from "react-bootstrap";
+
+export default function Disney(){
+    
+    const [filmes, setFilmes] = useState([])
+    
+    useEffect(()=>{
+         
+       apiMovie.get('movie/popular').then(resultado=>{
+            setFilmes(resultado.data.results)
+
+        })
+
+    }, [])
+    
+    return(
+        <>
+      
+      <Pagina titulo='Disney'>
+           
+         
+          {filmes.map(item=>(
+                           <p>{item.title}</p>
+                       ))}
+
+          <Row>
+    
+                {filmes.map(item => (
+                    <Col key={item.id} md={4} className="mb-4">
+                        <Card style={{ maxWidth:'250'}}>
+                                <Card.Img variant="top" src={'https://image.tmdb.org/t/p/w500/'+item.backdrop_path} />
+                                <Card.Body>
+                                    <Card.Title>{item.title}</Card.Title>
+                                    <Card.Text>
+                                        {item.tvShows}<br/>
+                                        {item.createdAt}<br/>
+                                        {item.updatedAt}<br/>
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                    </Col>
+                ))}
+            </Row>
+            </Pagina>
+          </> 
+      
+
+    )
+
+}
